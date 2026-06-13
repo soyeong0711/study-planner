173e9d58e4354eabbad9bb3d0ce5695c0a2880d3
war234 <hansub0529@gmail.com>
2026-06-14 01:09:01 +0900

    Initial commit

---
+++ app/src/main/java/com/yourdomain/moodin/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.yourdomain.moodin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.yourdomain.moodin.ui.theme.CharacterInteractionFeature
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    CharacterInteractionFeature()
+                }
+            }
+        }
+    }
+}
